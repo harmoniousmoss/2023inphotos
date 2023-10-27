b94837b4f572cee50ccb025ee298aa34f3d42ea9
Syaeful Bahri <99127787+harmoniousmoss@users.noreply.github.com>
2023-10-27 16:54:49 +0700

    Change CTA Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -61,7 +61,7 @@
 						class="inline-flex items-center justify-center px-6 py-3 text-base font-medium text-white transition-all duration-200 border border-white rounded-full hover:bg-white hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white focus:ring-offset-gray-900"
 						role="button"
 					>
-						Homepage
+						Home
 					</a>
 				</div>
 
